a06ea0462f32d93b41e2fac8b3c260638baf77b5
dafwa <daffafahir@gmail.com>
2025-12-07 08:31:11 +0800

    menambahkan form dan tampilan login serta routesnya, memperbaiki autentikasi login/logout dengan membuat controller, memperbaiki tampilan keseluruhan website

--- app/Http/Controllers/Auth/Logout.php
+++ app/Http/Controllers/Auth/Logout.php
@@ -13,10 +13,12 @@ class Logout extends Controller
      */
     public function __invoke(Request $request)
     {
-        $user = $request->user();
+        Auth::logout();
 
-        Auth::logout($user);
+        // Invalidate session
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
-        return redirect('/')->with('success', 'Kamu berhasil Logout!');
+        return redirect('/')->with('success', 'Kamu telah Logout.');
     }
 }
